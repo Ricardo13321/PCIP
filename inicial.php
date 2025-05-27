2035239ee261f3f322ab69f96c6de87467128ef3
guilherme359 <guilhermegpeluso@gmail.com>
2025-05-27 17:07:24 -0300

    Add files via upload

---
+++ inicial.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--ajuda o navegador a entender que a linguagem do site é pt-br-->
+    <meta http-equiv="content-language" content="pt-br">
+    <title>PHP / Array</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<style>
+    body {
+        background-color:rgb(231, 231, 231);
+        
+    }
+</style>
+<body>
+    <div class="p-4 mb-3" style="background:rgb(150, 64, 29)">
+        <center><h2><b>MENU INICIAL PCIP</b></h2></center>
+    </div>
+                <nav>
+                    &nbsp;&nbsp;
+                    <a href="inicial.php" style="color: black; text-decoration: none"> HOME  | </a>
+                    <a href="listagem.php" style="color: black; text-decoration: none"> LISTAGEM  | </a>
+                    <a href="gravar.php" style="color: black; text-decoration: none"> SALVAR DADOS</a>
+                    <div class="user">  
+                    <hr/>
+                </nav>
+        <br></br>
+    </div>
+    
+   <div class="row justify-content-center row-cols-2 row-cols-md-3 text-cente">
+        <div class="cols">
+            <div class="card mb-4 rounded shadow-sw">
+                <div class="card-body py-5">
+                </div>
+            </div>
+        </div>
+          <div class="cols">
+            <div class="card mb-4 rounded shadow-sw">
+                <div class="card-body py-5">
+                </div>
+            </div>
+        </div>
+          <div class="cols">
+            <div class="card mb-4 rounded shadow-sw">
+                <div class="card-body py-5">
+                </div>
+            </div>
+        </div>
+          <div class="cols">
+            <div class="card mb-4 rounded shadow-sw">
+                <div class="card-body py-5">
+                </div>
+            </div>
+        </div>
+        
+     </div>
+</body>
+</html>
