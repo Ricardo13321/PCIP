7de70983f29588cfa8acc6a7be241e764e06b10a
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-05-28 21:01:14 -0300

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,11 +7,10 @@
     <!--ajuda o navegador a entender que a linguagem do site é pt-br-->
     <meta http-equiv="content-language" content="pt-br">
     <title>PHP / Array</title>
-
+       
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <link type="text/css"  rel="stylesheet" href="estilo.css">
-</head>
 <style>
     body {
         background-color:rgb(231, 231, 231);
@@ -56,7 +56,8 @@
         <div class="d-flex shadow-lg navmenu">
             <?php include "menunav.html" ?>
         </div>
-        <div class="w-100" style="border: 1px solid black;">      
+        <div class="w-100" style="border: 1px solid black;">
+<!-- =====================================================================================  -->        
     <div class="container text-center">
   <div class="row">
     <div class="col" style="height: 240;">
@@ -71,19 +72,19 @@
     </div>
     <div class="col" style="height: 240;">
         <div class="p-1">
-       <?php include "grafico3.php"?>
+       <?php include "grafico4.php"?>
         </div>
     </div>
-    <div class="col">
+    <div class="col" style="height: 240;">
         <div class="p-1">
-       <?php include "grafico4.php"?>
-         </div>
+       <?php include "grafico3.php"?>
+        </div>
     </div>
   </div>
   <div class="row">
     <div class="col">
      <div class="p-1">
-      <?php include "grafico5.php"?>
+      graf5
         </div>
     </div>
     <div class="col">
@@ -104,6 +105,9 @@
         </div>
     </div>
   </div>
-</div>  
+</div>
+<!-- =====================================================================================  -->        
 </body>
 </html>
+
+
--- grafico4.php
+++ grafico4.php
@@ -6,28 +6,33 @@
       google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Pac Man', 'Percentage'],
-          ['', 75],
-          ['', 25]
+          ['Language', 'Speakers (in millions)'],
+          ['Assamese', 13], ['Bengali', 83], ['Bodo', 1.4],
+          ['Dogri', 2.3], ['Gujarati', 46], ['Hindi', 300],
+          ['Kannada', 38], ['Kashmiri', 5.5], ['Konkani', 5],
+          ['Maithili', 20], ['Malayalam', 33], ['Manipuri', 1.5],
+          ['Marathi', 72], ['Nepali', 2.9], ['Oriya', 33],
+          ['Punjabi', 29], ['Sanskrit', 0.01], ['Santhali', 6.5],
+          ['Sindhi', 2.5], ['Tamil', 61], ['Telugu', 74], ['Urdu', 52]
         ]);
 
         var options = {
+          title: 'Indian Language Use',
           legend: 'none',
-          pieSliceText: 'none',
-          pieStartAngle: 135,
-          tooltip: { trigger: 'none' },
-          slices: {
-            0: { color: 'yellow' },
-            1: { color: 'transparent' }
-          }
+          pieSliceText: 'label',
+          slices: {  4: {offset: 0.2},
+                    12: {offset: 0.3},
+                    14: {offset: 0.4},
+                    15: {offset: 0.5},
+          },
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('pacman'));
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
         chart.draw(data, options);
       }
     </script>
   </head>
   <body>
-    <div id="pacman" style="width: 100%; height: 100%; background-color:rgb(192, 192, 192);"></div>
+    <div id="piechart" style="width: 100%; height: 100%; background-color:rgb(192, 192, 192);"></div>
   </body>
 </html>
