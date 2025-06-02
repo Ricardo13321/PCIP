f981df92eff551feba725c1dfceca37fee1dd939
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-06-02 16:54:51 -0300

    Revert "Ricardo"

--- lista.php
+++ lista.php
@@ -1,35 +1,76 @@
-<?php
-date_default_timezone_set('America/Sao_Paulo');
 
-$nome = "Juan";
-$codigo = "32483";
-$quantidade = "30";
-$modelo = "Social";
-$status= "Produto final";
-
-echo "
-<tr>
-    <th>Funcionário</th>
-    <th>Código</th>
-    <th>Quantidade</th>
-    <th>Modelo</th>
-    <th>Estado</th>
-    <th>Data</th>
-    <th>Hora</th>
-</tr>
-";
-
-for ($i = 0; $i < 30; $i++) {
-    echo "
-        <tr>
-            <td>$nome</td>
-            <td>$codigo</td>
-            <td>$quantidade</td>
-            <td>$modelo</td>
-            <td>$status</td>
-            <td>" . date("d") . "/" . date("m") . "/" . date("Y") . "</td>
-            <td>" . date("H:i:s") . "</td>
-        </tr>
-    ";
-}
-?>
+        <table class="table table-bordered col-6 card-body" style="width: auto;height: auto;">
+            <tbody id="table">
+            <tr>
+                <th>Funcionário</th>
+                <th>Modelo</th>
+                <th>Estado</th>
+                <th>Data</th>
+                <th>Hora</th>
+                
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            <tr>
+                <td>Juan</td>
+                <td>Social</td>
+                <td>Perda</td>
+                <td>27/05/2025</td>
+                <td>09:10</td>
+            </tr>
+            </tbody>
+        </table>
